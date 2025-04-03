42c6e541d5b66fc1b5735dea45f532289c81864f
Naronitsu <damiannav03@gmail.com>
2025-04-03 20:22:37 +0200

    Adding Poll Model

--- DataAccess/PollDbContext.cs
+++ DataAccess/PollDbContext.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
 namespace DataAccess
 {
-    internal class PollDbContext
+    public class PollDbContext : DbContext
     {
+        public PollDbContext(DbContextOptions<PollDbContext> options) : base(options) { }
+
+        public DbSet<Poll> Polls { get; set; }
     }
 }
--- DataAccess/PollRepository.cs
+++ DataAccess/PollRepository.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Domain;
+
 namespace DataAccess
 {
-    internal class PollRepository
+    public class PollRepository
     {
+        private readonly PollDbContext _context;
+
+        public PollRepository(PollDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePoll(Poll poll)
+        {
+            poll.DateCreated = DateTime.Now;
+            _context.Polls.Add(poll);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Poll> GetPolls()
+        {
+            return _context.Polls.OrderByDescending(p => p.DateCreated).ToList();
+        }
     }
 }
