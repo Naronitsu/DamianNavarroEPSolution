107b4476ee5c00843056a89dae14a2674b9bbe28
Naronitsu <damiannav03@gmail.com>
2025-04-03 20:59:14 +0200

    DB connect, Views, controller, default page

---
+++ DamianNavarroEPSolution/Controllers/PollController.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class PollController : Controller
+    {
+        private readonly PollRepository _pollRepository;
+
+        public PollController(PollRepository pollRepository)
+        {
+            _pollRepository = pollRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var polls = _pollRepository.GetPolls();
+            return View(polls);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Poll poll)
+        {
+            if (ModelState.IsValid)
+            {
+                _pollRepository.CreatePoll(poll);
+                return RedirectToAction("Index");
+            }
+            return View(poll);
+        }
+    }
+}
