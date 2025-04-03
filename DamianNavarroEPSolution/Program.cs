3312cd6b1ceac3cfb93c777ac18d0bb8310d76c8
Naronitsu <damiannav03@gmail.com>
2025-04-03 20:46:33 +0200

    DB stuff

--- DamianNavarroEPSolution/Program.cs
+++ DamianNavarroEPSolution/Program.cs
@@ -1,4 +1,5 @@
 using DamianNavarroEPSolution.Data;
+using DataAccess;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,12 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<PollDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<PollRepository>();
+
+
 
 var app = builder.Build();
 
